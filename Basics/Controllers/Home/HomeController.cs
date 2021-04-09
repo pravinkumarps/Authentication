2d5fac84bee045baf753479f18505a53641c56a6
Praveen Kumar Penumarthi <pravinkumar.ps@hotmail.com>
2021-04-09 16:54:26 +0530

    Add project files.

---
+++ Basics/Controllers/Home/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basics.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var userClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Prav"),
+                new Claim(ClaimTypes.Email,"abc@abc.com")
+            };
+
+            var anotherClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Subrahmanya"),
+                new Claim("CustomClaim","SuperMan")
+            };
+
+            var userIdentity = new ClaimsIdentity(userClaims, "CustomIdentity");
+            var anotherIdentity = new ClaimsIdentity(anotherClaims, "OtherIdentity");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { userIdentity, anotherIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+            return RedirectToAction("Index");
+        }
+    }
+}
